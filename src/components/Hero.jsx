4870912011f28f7a88ae990f1204d04805cc04e0
Shubhradeep Das <shubhramail.das@gmail.com>
2024-11-18 03:51:39 +0530

    Phone screen optimizations

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,24 +1,72 @@
 import { Typography, Box } from "@mui/material";
-import React from "react";
+import { React, useState, useEffect } from "react";
 
 const Hero = () => {
+    const [isPhone, setIsPhone] = useState(false);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsPhone(window.innerWidth < 768);
+        };
+        window.addEventListener("resize", handleResize);
+        handleResize();
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+    }, []);
+    const divStyle = (isPhone) => {
+        if (isPhone) {
+            return {
+                padding: "30 0",
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+            };
+        } else {
+            return {
+                padding: "30 0",
+                display: "flex",
+                flexDirection: "row",
+            };
+        }
+    };
+    const imgStyle = (isPhone) => {
+        if (isPhone) {
+            return {
+                float: "none",
+                marginRight: "0",
+                borderRadius: "10%",
+            };
+        } else {
+            return {
+                float: "left",
+                marginRight: "2rem",
+                borderRadius: "10%",
+            };
+        }
+    };
+    const contentStyle = (isPhone) => {
+        if (isPhone) {
+            return {
+                marginTop: "1rem",
+                marginLeft: "0",
+            };
+        } else {
+            return {
+                marginLeft: "2rem",
+            };
+        }
+    };
     return (
-        <div style={{ padding: "30 0" }}>
+        <div style={divStyle(isPhone)}>
             <img
                 src="myimgnew.png"
                 alt="myimg"
-                width="40%"
-                style={{
-                    float: "left",
-                    marginRight: "2rem",
-                    borderRadius: "10%",
-                }}
+                width={isPhone ? "80%" : "40%"}
+                style={imgStyle(isPhone)}
             ></img>
-            <div
-                style={{
-                    marginLeft: "2rem",
-                }}
-            >
+            <div style={contentStyle(isPhone)}>
                 <div>
                     <Typography color="#deecfc" variant="h4">
                         <Box
--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -1,5 +1,15 @@
-import { AppBar, Box, Button, Card, Toolbar, Typography } from "@mui/material";
-import { React, useRef } from "react";
+import {
+    AppBar,
+    Box,
+    Button,
+    Card,
+    IconButton,
+    Menu,
+    Toolbar,
+    Typography,
+    MenuItem,
+} from "@mui/material";
+import { React, useRef, useState, useEffect } from "react";
 import Hero from "./Hero";
 import Projects from "./Projects";
 import About from "./About";
@@ -9,6 +19,7 @@ import Loader from "./loader/Loader";
 import Contact from "./Contact";
 import Footer from "./Footer";
 import PdfView from "./pdfopen/PdfView";
+import MenuIcon from "@mui/icons-material/Menu";
 
 const Page = () => {
     const heroRef = useRef(null);
@@ -17,6 +28,27 @@ const Page = () => {
     const skillRef = useRef(null);
     const journeyRef = useRef(null);
     const contactRef = useRef(null);
+    const [isPhone, setIsPhone] = useState(false);
+    const [anchorEl, setAnchorEl] = useState(null);
+    const open = Boolean(anchorEl);
+
+    const handleMenuOpen = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleMenuClose = () => {
+        setAnchorEl(null);
+    };
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsPhone(window.innerWidth < 768);
+        };
+        window.addEventListener("resize", handleResize);
+        handleResize();
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+    }, []);
 
     const onLoaded = () => {
         const loaderClass = document.getElementsByClassName("loading-page")[0];
@@ -25,6 +57,60 @@ const Page = () => {
             document.getElementsByClassName("home")[0].style.display = "block";
             loaderClass.classList.add("fadeout");
         }, 1000);
+    };
+    const displayStyle = (isPhone) => {
+        var phoneCard, aboutCard;
+        if (isPhone) {
+            phoneCard = {
+                backdropFilter: "blur(10px)",
+                background: "none",
+                width: "70%",
+                marginTop: "2rem",
+            };
+            aboutCard = {
+                backdropFilter: "blur(10px)",
+                background: "none",
+                width: "80%",
+                marginTop: "2rem",
+                padding: "1rem",
+            };
+        } else {
+            phoneCard = {
+                backdropFilter: "blur(10px)",
+                background: "none",
+                width: "70%",
+                marginTop: "2rem",
+                justifyContent: "center",
+            };
+            aboutCard = {
+                backdropFilter: "blur(10px)",
+                background: "none",
+                width: "70%",
+                marginTop: "2rem",
+                padding: "5rem",
+            };
+        }
+        return { phoneCard, aboutCard };
+    };
+
+    const timelineStyle = (isPhone) => {
+        if (isPhone) {
+            return {
+                backdropFilter: "blur(10px)",
+                background: "none",
+                width: "90%",
+                marginTop: "2rem",
+                paddingTop: "5rem",
+            };
+        } else {
+            return {
+                backdropFilter: "blur(10px)",
+                background: "none",
+                width: "70%",
+                marginTop: "2rem",
+                paddingTop: "5rem",
+            };
+        }
     };
     return (
         <div>
@@ -66,68 +152,166 @@ const Page = () => {
                                 shubhrad1
                             </Typography>
                         </Box>
-                        <Box sx={{ color: "white" }}>
-                            <Button color="inherit" onClick={PdfView}>
-                                Resume
-                            </Button>
-                            <Button
-                                color="inherit"
-                                onClick={() => {
-                                    aboutRef.current.scrollIntoView({
-                                        behavior: "smooth",
-                                        block: "center",
-                                    });
-                                }}
-                            >
-                                About
-                            </Button>
-                            <Button
-                                color="inherit"
-                                onClick={() => {
-                                    projectRef.current.scrollIntoView({
-                                        behavior: "smooth",
-                                        block: "center",
-                                    });
-                                }}
-                            >
-                                Projects
-                            </Button>
-                            <Button
-                                color="inherit"
-                                onClick={() => {
-                                    skillRef.current.scrollIntoView({
-                                        behavior: "smooth",
-                                        block: "center",
-                                    });
-                                }}
-                            >
-                                Skill
-                            </Button>
-                            <Button
-                                color="inherit"
-                                onClick={() => {
-                                    journeyRef.current.scrollIntoView({
-                                        behavior: "smooth",
-                                        block: "nearest",
-                                    });
-                                }}
-                            >
-                                Journey
-                            </Button>
-                            <Button
-                                color="inherit"
-                                onClick={() => {
-                                    contactRef.current.scrollIntoView({
-                                        behavior: "smooth",
-                                        block: "center",
-                                    });
-                                }}
-                            >
-                                Contact
-                            </Button>
-                        </Box>
+                        {isPhone ? (
+                            <>
+                                <IconButton
+                                    sx={{ marginRight: "0rem" }}
+                                    onClick={handleMenuOpen}
+                                >
+                                    <MenuIcon sx={{ color: "white" }} />
+                                </IconButton>
+
+                                <Menu
+                                    anchorEl={anchorEl}
+                                    open={open}
+                                    onClose={handleMenuClose}
+                                    slotProps={{
+                                        paper: {
+                                            sx: {
+                                                marginTop: "0.5rem",
+                                                backgroundColor:
+                                                    "rgba(0,0,0,0)",
+                                                backdropFilter: "blur(20px)", // Blurred effect
+                                                boxShadow: "none",
+                                                color: "white",
+                                                width: "100%",
+                                            },
+                                        },
+                                    }}
+                                >
+                                    <MenuItem
+                                        onClick={PdfView}
+                                        sx={{ justifySelf: "center" }}
+                                    >
+                                        Resume
+                                    </MenuItem>
+                                    <MenuItem
+                                        onClick={() => {
+                                            aboutRef.current.scrollIntoView({
+                                                behavior: "smooth",
+                                                block: "center",
+                                            });
+                                            handleMenuClose();
+                                        }}
+                                        sx={{ justifySelf: "center" }}
+                                    >
+                                        About
+                                    </MenuItem>
+                                    <MenuItem
+                                        onClick={() => {
+                                            projectRef.current.scrollIntoView({
+                                                behavior: "smooth",
+                                                block: "center",
+                                            });
+                                            handleMenuClose();
+                                        }}
+                                        sx={{ justifySelf: "center" }}
+                                    >
+                                        Projects
+                                    </MenuItem>
+                                    <MenuItem
+                                        onClick={() => {
+                                            skillRef.current.scrollIntoView({
+                                                behavior: "smooth",
+                                                block: "center",
+                                            });
+                                            handleMenuClose();
+                                        }}
+                                        sx={{ justifySelf: "center" }}
+                                    >
+                                        Skill
+                                    </MenuItem>
+                                    <MenuItem
+                                        onClick={() => {
+                                            journeyRef.current.scrollIntoView({
+                                                behavior: "smooth",
+                                                block: "nearest",
+                                            });
+                                            handleMenuClose();
+                                        }}
+                                        sx={{ justifySelf: "center" }}
+                                    >
+                                        Journey
+                                    </MenuItem>
+                                    <MenuItem
+                                        onClick={() => {
+                                            contactRef.current.scrollIntoView({
+                                                behavior: "smooth",
+                                                block: "center",
+                                            });
+                                            handleMenuClose();
+                                        }}
+                                        sx={{ justifySelf: "center" }}
+                                    >
+                                        Contact
+                                    </MenuItem>
+                                </Menu>
+                            </>
+                        ) : (
+                            <Box sx={{ color: "white" }}>
+                                <Button color="inherit" onClick={PdfView}>
+                                    Resume
+                                </Button>
+                                <Button
+                                    color="inherit"
+                                    onClick={() => {
+                                        aboutRef.current.scrollIntoView({
+                                            behavior: "smooth",
+                                            block: "center",
+                                        });
+                                    }}
+                                >
+                                    About
+                                </Button>
+                                <Button
+                                    color="inherit"
+                                    onClick={() => {
+                                        projectRef.current.scrollIntoView({
+                                            behavior: "smooth",
+                                            block: "center",
+                                        });
+                                    }}
+                                >
+                                    Projects
+                                </Button>
+                                <Button
+                                    color="inherit"
+                                    onClick={() => {
+                                        skillRef.current.scrollIntoView({
+                                            behavior: "smooth",
+                                            block: "center",
+                                        });
+                                    }}
+                                >
+                                    Skill
+                                </Button>
+                                <Button
+                                    color="inherit"
+                                    onClick={() => {
+                                        journeyRef.current.scrollIntoView({
+                                            behavior: "smooth",
+                                            block: "nearest",
+                                        });
+                                    }}
+                                >
+                                    Journey
+                                </Button>
+                                <Button
+                                    color="inherit"
+                                    onClick={() => {
+                                        contactRef.current.scrollIntoView({
+                                            behavior: "smooth",
+                                            block: "center",
+                                        });
+                                    }}
+                                >
+                                    Contact
+                                </Button>
+                            </Box>
+                        )}
                     </Toolbar>
                 </AppBar>
+
                 <div
                     style={{
                         display: "flex",
@@ -137,15 +321,8 @@ const Page = () => {
                     }}
                     ref={heroRef}
                 >
-                    <Card
-                        sx={{
-                            backdropFilter: "blur(10px)",
-                            background: "none",
-                            width: "50%",
-                            marginTop: "2rem",
-                        }}
-                    >
-                        <Hero />
+                    <Card sx={displayStyle(isPhone).phoneCard}>
+                        <Hero isPhone={isPhone} />
                     </Card>
                 </div>
                 <div
@@ -157,15 +334,7 @@ const Page = () => {
                     }}
                     ref={aboutRef}
                 >
-                    <Card
-                        sx={{
-                            backdropFilter: "blur(10px)",
-                            background: "none",
-                            width: "70%",
-                            marginTop: "2rem",
-                            padding: "5rem",
-                        }}
-                    >
+                    <Card sx={displayStyle(isPhone).aboutCard}>
                         <About />
                     </Card>
                 </div>
@@ -218,15 +387,7 @@ const Page = () => {
                     }}
                     ref={journeyRef}
                 >
-                    <Card
-                        sx={{
-                            backdropFilter: "blur(10px)",
-                            background: "none",
-                            width: "70%",
-                            marginTop: "2rem",
-                            paddingTop: "5rem",
-                        }}
-                    >
+                    <Card sx={timelineStyle(isPhone)}>
                         <MyTimeline />
                     </Card>
                 </div>
